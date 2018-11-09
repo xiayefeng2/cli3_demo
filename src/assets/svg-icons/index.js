81f09b4987336eb4e6bbd562bb2e5d7c06dead55
xiayefeng <xiayu_12@yeah.net>
2018-11-10 00:45:35 +0800

    update node-sass

--- src/assets/svg-icons/index.js
+++ src/assets/svg-icons/index.js
@@ -4,5 +4,4 @@ import Vue from 'vue'
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 const req = require.context('./icons', false, /\.svg$/)
 const iconMap = requireAll(req)
-
 Vue.prototype.$IconSvg = iconMap.map(e => e.default.id.slice(3))
