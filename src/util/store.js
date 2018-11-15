d683a2a8604e313ac103421313cd4d587bf7d45a
xiayefeng <xiayu_12@yeah.net>
2018-11-15 21:00:07 +0800

    add storage

---
+++ src/util/store.js
@@ -0,0 +1,107 @@
+import { isObject } from './index'
+
+export default function Store () {
+  this.getSession = key => {
+    /* if (!window.sessionStorage) {
+      console.log('当前浏览器不支持sessionStorage！')
+      throw new Error('浏览器不支持')
+    } */
+    this.checkBrowser()
+    let val = this.getItem(key)
+    if (val === null) {
+      console.log(key + '：sessionStorage值 不存在')
+      return ''
+    } else {
+      return this.getValue(val)
+    }
+  }
+  this.getLocal = (key) => {
+    this.checkBrowser()
+    let val = this.getItem(key, 2)
+    if (val === null) {
+      console.log(key + '：localStorage值 不存在')
+      return ''
+    } else {
+      return this.getValue(val)
+    }
+  }
+
+  this.setSession = (key, val) => {
+    this.setItem(key, val)
+  }
+
+  this.setLocal = (key, val) => {
+    this.setItem(key, val, 2)
+  }
+
+  this.checkBrowser = () => {
+    if (!window.sessionStorage || !window.localStorage) {
+      console.log('当前浏览器不支持sessionStorage！')
+      throw new Error('浏览器不支持')
+    }
+  }
+  this.getItem = (key, lx = 1) => {
+    let val
+    if (lx === 1) {
+      val = sessionStorage.getItem(key)
+    } else if (lx === 2) {
+      val = localStorage.getItem(key)
+    }
+    return val
+  }
+  this.removeItem = (key, lx = 1) => {
+    if (lx === 1) {
+      sessionStorage.removeItem('key')
+    } else {
+      localStorage.removeItem('key')
+    }
+  }
+  this.removeAll = (lx = 1) => {
+    if (lx === 1) {
+      sessionStorage.clear()
+    } else {
+      localStorage.clear()
+    }
+  }
+  this.getValue = (val) => {
+    if (val === '') return ''
+    if (val === 'true' || val === 'false' || val === 'null' || val === 'undefind' || val === 'NaN') {
+      if (val === 'false') {
+        return false
+      }
+      if (val === 'null') {
+        return null
+      }
+      if (val === 'undefind') {
+        return void 0
+      }
+      if (val === 'NaN') {
+        return NaN
+      }
+      return true
+    }
+    let val2 = +val
+    if (typeof val2 === 'number' && !Number.isNaN(val2)) {
+      return val2
+    } else if (Number.isNaN(val2)) {
+      try {
+        let obj = JSON.parse(val)
+        if (isObject(obj)) {
+          return obj
+        }
+      } catch (e) {
+        if (e.includes('in JSON')) {
+          return val
+        }
+      }
+    }
+  }
+  this.setItem = (key, val, lx = 1) => {
+    if (lx === 1) {
+      if (typeof val === 'object' && val !== null) {
+        val = JSON.stringify(val)
+      }
+      sessionStorage.setItem(key, val)
+    }
+  }
+}
