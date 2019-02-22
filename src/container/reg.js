31b4b921e57a96369594158db13d2bfccc552191
xiayefeng <xiayu_12@yeah.net>
2019-02-22 18:02:13 +0800

    Update reg.js

--- src/container/reg.js
+++ src/container/reg.js
@@ -4,7 +4,8 @@ const reg = {
   tel: /^1(?:[38][0-9]|4[579]|5[012356789]|66|7[1235678]|9[89])\d{8}$/,
   cn: /^[\u4e00-\u9fa5]{0,}$/, // 匹配汉字
   post: /[1-9]\d{5}(?!\d)/, // 邮编
-  emoji: /\ud83c[\udf00-\udfff]|\ud83d[\udc00-\ude4f]|\ud83d[\ude80-\udeff]/g // 表情符号
+  emoji: /\ud83c[\udf00-\udfff]|\ud83d[\udc00-\ude4f]|\ud83d[\ude80-\udeff]/g, // 表情符号
+  numCut: /\B(?=(\d{3})+\b)/g // 用逗号隔开数字，例如 12345678 123456789" => "12,345,678 123,456,789"
 }
 
 export default reg
