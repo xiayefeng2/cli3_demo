7bfd690bafc9eee4922dbab796e05ef290a7a678
xiayefeng <xiayu_12@yeah.net>
2019-07-29 00:55:08 +0800

    Update index.js

--- src/route/index.js
+++ src/route/index.js
@@ -6,59 +6,60 @@ import utils from '@/utils'
 Vue.use(Router)
 
 const router = new Router({
-	mode: 'history',
-	routes: [
-		{
-			path: '/',
-			name: 'home',
-			components: {
-				default: Home,
-				head: () => import('@/views/TopHeader.vue'),
-				'left-nav': () => import('@/views/LeftNav.vue')
-			}
-		},
-		{
-			path: '/about',
-			name: 'about',
-			// route level code-splitting
-			// this generates a separate chunk (about.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			components: {
-				default: () =>
-					import(/* webpackChunkName: "about" */ '@/views/About.vue')
-			}
-		},
-		{
-			path: '/login',
-			name: 'login',
-			component: () => import('@/views/login.vue')
-		},
-		{
-			path: '/table',
-			name: 'table',
-			component: () => import('@/views/Table.vue')
-		}
-	]
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      components: {
+        default: Home,
+        head: () => import('@/views/TopHeader.vue'),
+        'left-nav': () => import('@/views/LeftNav.vue')
+      }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      components: {
+        default: () =>
+          import(/* webpackChunkName: "about" */ '@/views/About.vue')
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/login.vue')
+    },
+    {
+      path: '/table',
+      name: 'table',
+      component: () => import('@/views/Table.vue')
+    }
+  ]
 })
 router.beforeEach((to, from, next) => {
-	if (to.matched.some(r => r.meta.requiresAuth)) {
-		const token = utils.cookies.get('token')
-		if (token && token !== 'undefined') {
-			next()
-		} else {
-			next({
-				name: 'login'
-			})
-		}
-	} else {
-		next()
-	}
+  if (to.matched.some(r => r.meta.requiresAuth)) {
+    const token = utils.cookies.get('token')
+    if (token && token !== 'undefined') {
+      next()
+    } else {
+      next({
+        name: 'login'
+      })
+    }
+  } else {
+    // router.push('/login')
+    next()
+  }
 })
 
 router.afterEach((to, from) => {
-	if (to.meta.title) {
-		utils.title(to.meta.title)
-	}
+  if (to.meta.title) {
+    utils.title(to.meta.title)
+  }
 })
 
 export default router
