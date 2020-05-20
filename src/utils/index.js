ed1bebdcbd242f7c8d6e2aec00e9962606157336
xiayefeng <xiayu_12@yeah.net>
2020-05-20 18:07:56 +0800

    feat(utils): add strHash method

--- src/utils/index.js
+++ src/utils/index.js
@@ -1018,4 +1018,26 @@ export function addScript (url) {
   return script
 }
 
+export function strHash (input) {
+  var I64BIT_TABLE =
+ 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-'
+  var hash = 5381
+  var i = input.length - 1
+
+  if (typeof input === 'string') {
+    for (; i > -1; i--) { hash += (hash << 5) + input.charCodeAt(i) }
+  } else {
+    for (; i > -1; i--) { hash += (hash << 5) + input[i] }
+  }
+  var value = hash & 0x7FFFFFFF
+
+  var retValue = ''
+  do {
+    retValue += I64BIT_TABLE[value & 0x3F]
+  }
+  while ((value >>= 6))
+
+  return retValue
+}
+
 export default utils
