9552cf11e84cdb547d82d170250794a6fb8313c2
xiayefeng <xiayu_12@yeah.net>
2020-09-06 16:53:23 +0800

    add feature

---
+++ src/components/Example.vue
@@ -0,0 +1,13 @@
+<template functional>
+  <div class="list">
+    <div
+      class="item"
+      v-for="item in props.list"
+      :key="item.id"
+      @click="props.itemClick(item)"
+    >
+      <p>{{ item.title }}</p>
+      <p>{{ item.content }}</p>
+    </div>
+  </div>
+</template>
---
+++ src/components/Message.vue
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Message from './HelloWorld.vue'
+
+// 构造子类
+let MessageConstructor = Vue.extend(Message)
+// 实例化组件
+let messageInstance = new MessageConstructor()
+// $mount可以传入选择器字符串，表示挂载到该选择器
+// 如果不传入选择器，将渲染为文档之外的的元素，你可以想象成 document.createElement()在内存中生成dom
+messageInstance.$mount()
+// messageInstance.$el获取的是dom元素
+document.body.appendChild(messageInstance.$el)
