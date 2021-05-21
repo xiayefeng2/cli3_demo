1b64042866a3af3f6fc1d5865b0df05bc6ce7e27
xiayefeng <xiayu_12@yeah.net>
2021-05-21 23:37:33 +0800

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -737,6 +737,28 @@ utils.memoize = function (func, hasher) {
   return memoize
 }
 
+export function memoize (func, resolver) {
+  if (typeof func !== 'function' || (resolver != null && typeof resolver !== 'function')) {
+    throw new TypeError('Expected a function')
+  }
+  !memoize.clear && (memoize.clear = function () {
+    memoized.cache.clear()
+  })
+  const memoized = function (...args) {
+    const key = resolver ? resolver.apply(this, args) : args[0]
+    const cache = memoized.cache
+
+    if (cache.has(key)) {
+      return cache.get(key)
+    }
+    const result = func.apply(this, args)
+    memoized.cache = cache.set(key, result) || cache
+    return result
+  }
+  memoized.cache = new Map()
+  return memoized
+}
+
 // 加入收藏夹
 export function addFavorite (sURL, sTitle) {
   try {
